56f115a65dfcbdb26d10246f2703a8a52d02f7dc
ASmallYawn <101952296+ASmallYawn@users.noreply.github.com>
2022-03-30 04:45:24 +0000

    final server

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ from flask_cors import CORS
 from flask_cors import cross_origin
 app = Flask(__name__)
 CORS(app)
-sfToken = os.environ['sfToken']
+Token = os.environ['Token']
 
-def updateDB():         
+#San Francisco
+def updateDBSF():         
   headers = {
-    "X-App Token": sfToken,
+    "X-App Token": Token,
     "limit": "500000"
   }
   
@@ -16,27 +17,64 @@ def updateDB():
   r = requests.get(url, headers=headers)
   data = r.json()
   
-  dataSorted = {}
+  #dataSorted = {}
   
-  for value in data:
-    id = value["business_name"] 
-    if id in dataSorted:
-      dataSorted[id].append(value)
-    else:
-      dataSorted[id] = [value]
+  #for value in data:
+  #  id = value["business_name"] 
+  #  if id in dataSorted:
+  #    dataSorted[id].append(value)
+  #  else:
+  #    dataSorted[id] = [value]
 
-  replit.db["data"] = json.dumps(data)
+  replit.db["sf"] = json.dumps(data)
 
-updateDB()
+#Chicago
+def updateDBCH():         
+  headers = {
+    "X-App Token": Token,
+    "limit": "500000"
+  }
+  
+  url = "https://data.cityofchicago.org/resource/4ijn-s7e5.json"
+  r = requests.get(url, headers=headers)
+  data = r.json()
+  
+  #dataSorted = {}
+  
+  #for value in data:
+  #  id = value["dba_name"] 
+  #  if id in dataSorted:
+  #    dataSorted[id].append(value)
+  #  else:
+  #    dataSorted[id] = [value]
 
-def getDB():
-  d = replit.db["data"]
+  replit.db["ch"] = json.dumps(data)
+
+updateDBSF()
+updateDBCH()
+
+
+
+#GET Responses
+#SanFrancisco
+def getDBSF():
+  d = replit.db["sf"]
   return(d)
 
-@app.route('/')
+@app.route('/sf')
 @cross_origin()
-def e():
-  return getDB()
+def sf():
+  return getDBSF()
+
+#Chicago
+def getDBCH():
+  d = replit.db["ch"]
+  return(d)
+
+@app.route('/ch')
+@cross_origin()
+def ch():
+  return getDBCH()
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0")  
